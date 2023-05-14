677a664539488c9e9740600add8b68e25312b341
Liel Yoash <118906496+LielYoash@users.noreply.github.com>
2023-05-14 16:53:59 +0300

    Add files via upload

--- Receiver.py
+++ Receiver.py
@@ -1,8 +1,6 @@
 import io
 import socket
-import sys
 import time
-import os
 
 SERVER_ADDR = ("127.0.0.1", 9090)
 FILE = "received"
@@ -10,75 +8,72 @@ TIMEOUT = 0.001
 CONTINUE_MSG = b"Continue"
 EXIT_MSG = b"Exit"
 EXIT_CODES = (0, 1)
-AUTH = f"{int(1763 ^ 1818)}".encode()
-BUFFER_SIZE = io.DEFAULT_BUFFER_SIZE + sys.getsizeof(b"$") + max(sys.getsizeof(b"N"), sys.getsizeof(b"F"))
+AUTH = f"{int(5965 ^ 7788)}".encode()
+BUFFER_SIZE = io.DEFAULT_BUFFER_SIZE
 CC = (b"reno", b"cubic")
 NUM_CONNECTIONS = 300
 SPLITTER = b"$"
 FINAL_SEGMENT = b"F"
 NOT_FINAL_SEGMENT = b"N"
-CC = (b"reno", b"cubic")
+CONT = b"liel"
+renu_times = []
+cubic_times = []
 
 
 def main():
-
-    renu_times = []
-    cubic_times = []
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP) as sock:
         sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_CONGESTION, CC[0])
         sock.bind(SERVER_ADDR)
         sock.listen(NUM_CONNECTIONS)
+        client, addr = sock.accept()
 
         while True:
-            client, addr = sock.accept()
+            start = time.time()
+            with open(FILE, "wb") as file:
+                while True:
+                    data = client.recv(io.DEFAULT_BUFFER_SIZE)
+                    if data.__contains__(CONT):
+                        file.write(data)
+                        break
+                    file.write(data)
+            end = time.time()
+            print("First part has been received.")
+            renu_times.append(float(end - start))
 
-            while True:
+            client.sendall(AUTH)
+            time.sleep(TIMEOUT)
+            print("Authentication has been sent.")
 
-                start = time.time()
-                with open("fil1", "wb") as file:
-                    while True:
-                        data = client.recv(io.DEFAULT_BUFFER_SIZE)
-                        if data.split(SPLITTER)[0] == FINAL_SEGMENT:
-                            break
-                        elif data.split(SPLITTER)[0] == NOT_FINAL_SEGMENT:
-                            file.write(data.split(SPLITTER)[1])
-                end = time.time()
-                print("First part has been received.")
-                renu_times.append(float(end - start))
+            sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_CONGESTION, CC[1])
 
-                client.sendall(AUTH)
-                time.sleep(TIMEOUT)
-                print("Authentication has been sent.")
+            start = time.time()
+            with open(FILE, "wb") as file:
+                while True:
+                    data = client.recv(io.DEFAULT_BUFFER_SIZE)
+                    if data.__contains__(CONT):
+                        file.write(data)
+                        break
+                    file.write(data)
+            end = time.time()
+            print("Second part has been received.")
+            cubic_times.append(float(end - start))
 
-                sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_CONGESTION, CC[1])
 
-                start = time.time()
-                with open("file2", "wb") as file:
-                    while True:
-                        data = client.recv(io.DEFAULT_BUFFER_SIZE)
-                        if data.split(SPLITTER)[0] == FINAL_SEGMENT:
-                            break
-                        elif data.split(SPLITTER)[0] == NOT_FINAL_SEGMENT:
-                            file.write(data.split(SPLITTER)[1])
-                end = time.time()
-                print("Second part has been received.")
-                cubic_times.append(float(end - start))
+            print("Waiting for data from the client...")
+            x = client.recv(io.DEFAULT_BUFFER_SIZE)
+            print("Data received")
+            if x == CONTINUE_MSG:
+                sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_CONGESTION, CC[0])
 
-                x = client.recv(io.DEFAULT_BUFFER_SIZE)
-                if x == CONTINUE_MSG:
-                    sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_CONGESTION, CC[0])
-                    continue
-                elif x == EXIT_MSG:
-                    client.close()
+            elif x == EXIT_MSG:
+                print("####################-RESULTS-####################")
+                for i in range(len(renu_times)):
+                    print(f"seq={i} reno={renu_times[i]} cubic={cubic_times[i]}")
+                client.close()
+                break
 
-                    print("####################-RESULTS-####################")
-                    for i in range(len(renu_times)):
-                        print(f"seq={i} reno={renu_times[i]} cubic={cubic_times[i]}")
-                    break
-    socket.shutdown()
-    socket.close()
 
 if __name__ == "__main__":
     main()
--- Sender.py
+++ Sender.py
@@ -4,7 +4,6 @@ import os
 import sys
 import time
 
-
 SERVER_ADDR = ("127.0.0.1", 9090)
 FILE = ("file", os.path.getsize("file"))
 FIRST_HALF = "first"
@@ -13,14 +12,14 @@ TIMEOUT = 0.001
 CONTINUE_MSG = b"Continue"
 EXIT_MSG = b"Exit"
 EXIT_CODES = (0, 1)
-AUTH = f"{int(1763 ^ 1818)}".encode()
+AUTH = f"{int(5965 ^ 7788)}".encode()
 BUFFER_SIZE = io.DEFAULT_BUFFER_SIZE
 CC = (b"reno", b"cubic")
+CONT = b"liel"
 
 
 def divide_file():
     with open(FILE[0], "rb") as file:
-
         with open(FIRST_HALF, "wb") as f1:
             f1.write(file.read(FILE[1]))
 
@@ -34,14 +33,16 @@ def main():
     divide_file()
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP) as sock:
-        sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_CONGESTION, CC[0])
         sock.connect(SERVER_ADDR)
 
         while True:
+            sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_CONGESTION, CC[0])
 
             with open(FIRST_HALF, "rb") as file:
-                data = file.read(BUFFER_SIZE)
-                while data:
+                while True:
+                    data = file.read(BUFFER_SIZE)
+                    if not data:
+                        break
                     sock.sendall(f"N${data.decode()}".encode())
                     time.sleep(TIMEOUT)
                     data = file.read(BUFFER_SIZE)
@@ -49,17 +50,21 @@ def main():
                 time.sleep(TIMEOUT)
             print("First half has been sent.")
 
+            sock.sendall(CONT)
+
             data = sock.recv(BUFFER_SIZE)
             if data != AUTH:
-                print("Authentication doesnt match.")
+                print("Authentication doesn't match.")
                 sys.exit(EXIT_CODES[1])
             print("Received Authentication.")
 
             sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_CONGESTION, CC[1])
 
             with open(SECOND_HALF, "rb") as file:
-                data = file.read(BUFFER_SIZE)
-                while data:
+                while True:
+                    data = file.read(BUFFER_SIZE)
+                    if not data:
+                        break
                     sock.sendall(f"N${data.decode()}".encode())
                     time.sleep(TIMEOUT)
                     data = file.read(BUFFER_SIZE)
@@ -67,28 +72,22 @@ def main():
                 time.sleep(TIMEOUT)
             print("Second half has been sent.")
 
+            sock.sendall(CONT)
+
             user_decision = input("Send again? (y/n) ")
             while user_decision.lower() != "y" and user_decision.lower() != "n":
                 user_decision = input("Send again? [enter 'y' or 'n' only!] ")
 
             if user_decision.lower() == "y":
                 sock.sendall(CONTINUE_MSG)
-                time.sleep(TIMEOUT)
 
                 sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_CONGESTION, CC[0])
-
                 continue
-            else:
-                sock.sendall(EXIT_MSG)
-                time.sleep(TIMEOUT)
-                sock.shutdown(socket.SHUT_RDWR)
-                # os.remove("first")
-                # os.remove("second")
 
-                print("Exiting...")
-                sys.exit(EXIT_CODES[0])
-    socket.shutdown()
-    socket.close()
+            elif user_decision.lower() == "n":
+                sock.sendall(EXIT_MSG)
+                sock.shutdown(socket.SHUT_RDWR)
+                break
 
 if __name__ == "__main__":
     main()
